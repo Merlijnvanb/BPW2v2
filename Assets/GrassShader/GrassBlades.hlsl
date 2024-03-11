686d2e717579d489293cf870ce921d4d14c7ec65
Merlijnvanb <merlijnvanballegooijen@outlook.com>
2024-03-12 00:51:37 +0100

    GrassFixCommit

--- Assets/GrassShader/GrassBlades.hlsl
+++ Assets/GrassShader/GrassBlades.hlsl
@@ -27,10 +27,13 @@ struct VertexOutput {
 float4 _BaseColor;
 float4 _TipColor;
 
-
 VertexOutput Vertex(uint vertexID: SV_VertexID) {
+    // Initialize the output struct
     VertexOutput output = (VertexOutput)0;
 
+    // Get the vertex from the buffer
+    // Since the buffer is structured in triangles, we need to divide the vertexID by three
+    // to get the triangle, and then modulo by 3 to get the vertex on the triangle
     DrawTriangle tri = _DrawTriangles[vertexID / 3];
     DrawVertex input = tri.vertices[vertexID % 3];
 
@@ -43,12 +46,14 @@ VertexOutput Vertex(uint vertexID: SV_VertexID) {
 }
 
 half4 Fragment(VertexOutput input) : SV_Target {
+    // Gather some data for the lighting algorithm
     InputData lightingInput = (InputData)0;
     lightingInput.positionWS = input.positionWS;
-    lightingInput.normalWS = input.normalWS;
-    lightingInput.viewDirectionWS = GetViewDirectionFromPosition(input.positionWS);
+    lightingInput.normalWS = input.normalWS; // No need to normalize, triangles share a normal
+    lightingInput.viewDirectionWS = GetViewDirectionFromPosition(input.positionWS); // Calculate the view direction
     lightingInput.shadowCoord = CalculateShadowCoord(input.positionWS, input.positionCS);
 
+    // Lerp between the base and tip color based on the blade height
     float colorLerp = input.uv;
     float3 albedo = lerp(_BaseColor.rgb, _TipColor.rgb, input.uv);
 
