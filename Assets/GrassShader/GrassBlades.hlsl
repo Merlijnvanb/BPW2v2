7d2007bf1d3ba862bb1e5d3a7819b762832e03fc
Merlijnvanb <merlijnvanballegooijen@outlook.com>
2024-03-15 14:16:04 +0100

    SmolUpdate

--- Assets/GrassShader/GrassBlades.hlsl
+++ Assets/GrassShader/GrassBlades.hlsl
@@ -28,8 +28,19 @@ float4 _BaseColor;
 float4 _TipColor;
 float4 _SpecularColor;
 
-half4 CalculateSpecular(Light light, float3 normal, float3 viewDir) {
-    return saturate(dot(normal, normalize(light.direction + viewDir)));
+float _AmbientIntensity;
+float4 _vecAmbient;
+
+float _DiffuseIntensity;
+
+float _SpecularPower;
+float _SpecularIntensity;
+
+float CalculateSpecular(Light light, float3 normal, float3 viewDir) {
+    float3 R = normalize(2 * dot(normal, light.direction) * normal - light.direction); 
+    float Specular = pow(saturate(dot(R, normalize(viewDir))), 1) * 1; 
+
+    return Specular;
 }
 
 VertexOutput Vertex(uint vertexID: SV_VertexID) {
@@ -51,20 +62,34 @@ VertexOutput Vertex(uint vertexID: SV_VertexID) {
 }
 
 half4 Fragment(VertexOutput input) : SV_Target {
-    float3 normal = input.normalWS;
+    float3 InNormal = input.normalWS;
     
-    if(dot(normal, float3(0,1,0) < 0)) normal = -normal; // Flip the normal if it's facing the wrong way
+    if(dot(InNormal, float3(0,1,0) < 0)) InNormal = -InNormal; // Flip the normal if it's facing the wrong way
     
-    float light = dot(normal, GetMainLight().direction) * 0.5 + 0.5f;
-    float3 viewDir = GetViewDirectionFromPosition(input.positionWS);
+    float light = dot(InNormal, GetMainLight().direction) * 0.5 + 0.5f;
+    float3 viewDir = GetViewDirectionFromPosition(_WorldSpaceCameraPos);
     Light mainLight = GetMainLight();
 
-    //AO
-    float maxHeight = 2.0f;
-    light = (input.uv / maxHeight) * light;
+    // //AO
+     float maxHeight = 2.0f;
+     light = (input.uv / maxHeight) * light;
     
 
-    return lerp(_BaseColor, _TipColor, light) + CalculateSpecular(mainLight, normal, viewDir);
+    // return lerp(_BaseColor, _TipColor, light) + (CalculateSpecular(mainLight, normal, viewDir) * _SpecularColor);
+
+    // Calculate the ambient term: 
+  float4 Ambient = _AmbientIntensity * _vecAmbient; 
+  // Calculate the diffuse term: 
+    InNormal = normalize(InNormal); 
+    float4 Diffuse = _DiffuseIntensity * float4(mainLight.color.r, mainLight.color.g, mainLight.color.b, 1) * saturate(dot(-mainLight.direction, InNormal)); 
+  // Fetch the pixel color from the color map: 
+    float4 Color = lerp(_BaseColor, _TipColor, light); 
+  // Calculate the reflection vector: 
+    float3 R = normalize(2 * dot(InNormal, -mainLight.direction) * InNormal + mainLight.direction); 
+  // Calculate the speculate component: 
+    float Specular = pow(saturate(dot(R, normalize(viewDir))), _SpecularPower) * _SpecularIntensity; 
+  // Calculate final color: 
+    return (Ambient + Diffuse + Specular) * Color; 
 }
 
 #endif
--- Assets/GrassShader/GrassBlades.shader
+++ Assets/GrassShader/GrassBlades.shader
@@ -5,6 +5,14 @@ Shader "Grass/GrassBlades"
         _BaseColor("Base color", Color) = (0, 0.5, 0, 1)
         _TipColor("Tip color", Color) = (0, 1, 0, 1)
         _SpecularColor("Specular color", Color) = (1, 1, 1, 1)
+
+        _AmbientIntensity("Ambient intensity", Float) = 1.0
+        _vecAmbient("Ambient color", Color) = (0.3, 0.3, 0.3, 1)
+
+        _DiffuseIntensity("Diffuse intensity", Float) = 1.0
+
+        _SpecularPower("Specular power", Float) = 8.0
+        _SpecularIntensity("Specular intensity", Float) = 2.0
     }
     SubShader
     {
